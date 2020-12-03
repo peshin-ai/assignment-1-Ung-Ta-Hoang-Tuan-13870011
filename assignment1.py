790c9b8c232d99d72aa9a0a5990e6abe5df78b9c
hoangtuanvp2011 <t00aienl@gmail.com>
2020-12-03 09:27:30 +0700

    changing the assignment file

--- assignment1.py
+++ assignment1.py
@@ -5,17 +5,24 @@ Date started: 29/11/2020
 GitHub URL: https://github.com/peshin-ai/assignment-1-Ung-Ta-Hoang-Tuan-13870011
 """
 import csv
+
+
 def Close():
     print("bye bye")
+
+
 def WatchedMovies():
     pass
+
+
 def AddMovies():
     pass
+
+
 def LoadMovies():
-    RawFile = open('movies.csv', 'r')
-    Csv_File = csv.reader(RawFile, delimiter=',')
-    Line = len(list(Csv_File))
-    print(Line)
+    pass
+
+
 def Process(char):
     if char == "L":
         LoadMovies()
@@ -25,11 +32,14 @@ def Process(char):
         WatchedMovies()
     else:
         Close()
+
+
 def Option():
     char = input().upper()
-    while char not in ["L","A","W","Q"]:
+    while char not in ["L", "A", "W", "Q"]:
         char = input()
     Process(char)
+
 
 def Menu():
     print("L - List movies")
@@ -38,19 +48,20 @@ def Menu():
     print("Q- Quit")
     Option()
 
+
 def main():
     """..."""
     print("Movies To Watch 1.0 - by Ung Ta Hoang Tuan")
     # TODO changing it
-    #global RawFile
+    global Line, Csv_File
     RawFile = open('movies.csv', 'r')
-
     Csv_File = csv.reader(RawFile, delimiter=',')
     Line = len(list(Csv_File))
     print("{} movies loaded".format(Line))
     Menu()
 
     RawFile.close()
+
+
 if __name__ == '__main__':
     main()
-
